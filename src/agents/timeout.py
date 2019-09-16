c153ff63f0e72c470f43706230285f973219c3f0
lccasagrande <lcamelocasagrande@gmail.com>
2019-09-16 15:38:34 -0300

    clean up

--- src/agents/timeout.py
+++ src/agents/timeout.py
@@ -12,120 +12,50 @@ from gridgym.envs.grid_env import GridEnv
 from gridgym.envs.simulator.resource import PowerStateType
 
 
-class TimeoutWithOracleAgent(Agent):
-    def __init__(self, nb_resources, nb_nodes, t_turnoff, t_turnon, min_idle_time=None):
-        self.t_turnoff = t_turnoff
-        self.t_turnon = t_turnon
-        self.nb_resources = nb_resources
-        self.nodes_state = np.zeros(shape=nb_nodes)
-        self.current_time = 0
-        self.time_to_compensate_turning_off = min_idle_time
-
-    def get_resources_that_compensate_turning_off(self, nb_resources, info):
-        if not self.time_to_compensate_turning_off:
-            nodes = info['nodes_profile']
-            sleep_power = sum(
-                ps['min'] for n in nodes for ps in n if ps['type'] == PowerStateType.sleep)
-            idle_power = sum(
-                ps['min'] for n in nodes for ps in n if ps['type'] == PowerStateType.computation)
-            turnon_power = sum(
-                ps['min'] for n in nodes for ps in n if ps['type'] == PowerStateType.switching_on)
-            turnoff_power = sum(
-                ps['min'] for n in nodes for ps in n if ps['type'] == PowerStateType.switching_off)
-            self.time_to_compensate_turning_off = (
-                (turnoff_power * self.t_turnoff) + (turnon_power * self.t_turnon) - sleep_power) / (idle_power - sleep_power)
-
-        for j in info['next_submissions']:
-            if nb_resources <= 0:
-                break
-            if j['subtime'] - self.current_time <= self.time_to_compensate_turning_off:
-                nb_resources -= j['res']
-        return max(0, nb_resources)
-
-    def act(self, obs, info=None):
-        reservation_size = 0
-        nb_available = sum(1 if a == 0 else 0 for a in obs['agenda'])
-        queue = list(itertools.takewhile(lambda j: j[0] != -1, obs['queue']))
-        if len(queue) > 0:
-            _, res, _, pjob_expected_time_to_start = queue[0]
-            if pjob_expected_time_to_start == 0:
-                nb_available -= res
-                pjob_expected_time_to_start = -1
-                queue = queue[1:]
-            for (_, res, walltime, _) in queue:
-                if pjob_expected_time_to_start == -1 and res <= nb_available:
-                    nb_available -= res
-                elif res <= nb_available and walltime <= pjob_expected_time_to_start:
-                    nb_available -= res
-        if info:
-            nb_available = self.get_resources_that_compensate_turning_off(
-                nb_available, info)
-
-        for i, node in enumerate(obs['platform']):
-            if nb_available >= len(node) and all(r == 0 or r == 2 or r == 3 for r in node):
-                reservation_size += 1
-                nb_available -= len(node)
-
-        self.current_time = obs['time']
-        return reservation_size
-
-    def play(self, env, render=False, verbose=False):
-        self.current_time = 0
-        obs, done, score, info = env.reset(), False, 0, None
-        while not done:
-            if render:
-                env.render()
-            obs, reward, done, info = env.step(self.act(obs, info))
-            score += reward
-        if verbose:
-            results = pd.read_csv(os.path.join(GridEnv.OUTPUT, '_schedule.csv')).to_dict('records')[0]
-            results['score'] = score
-            m = " - ".join("[{}: {}]".format(k, v) for k, v in results.items())
-            print("[RESULTS] {}".format(m))
-
-        plot_simulation_graphics(GridEnv.OUTPUT, show=True)
-        env.close()
-        return score
-
-
 class TimeoutAgent(Agent):
-    def __init__(self, timeout, nb_resources, nb_nodes):
-        assert timeout > 0
+    def __init__(self, timeout, nb_resources, nb_nodes, queue_sz):
+        assert timeout >= 0
         self.timeout = timeout
         self.nb_resources = nb_resources
         self.nodes_state = np.zeros(shape=nb_nodes)
         self.current_time = 0
+        self.queue_sz = queue_sz
+
+    def _get_available_resources(self, obs):
+        nb_available = sum(1 if a == 0 else 0 for a in obs['agenda'])
+        queue = obs['queue'][:self.queue_sz]
+        if len(queue) > 0:
+            (sub, res, wall, pjob_expected_t_start, user, profile) = queue[0]
+            if pjob_expected_t_start == 0 or res <= nb_available:
+                nb_available -= res
+                pjob_expected_t_start = -1
+                queue = queue[1:]
+            for (sub, res, wall, expected_t_start, user, profile) in queue:
+                if pjob_expected_t_start == -1 and res <= nb_available:
+                    nb_available -= res
+                elif res <= nb_available and wall <= pjob_expected_t_start:
+                    nb_available -= res
+        return nb_available
 
     def act(self, obs):
-        nb_available = sum(1 if a == 0 else 0 for a in obs['agenda'])
-        queue = obs['queue']
-        if len(queue) > 0:
-            _, res, _, pjob_expected_time_to_start, _ = queue[0]
-            if pjob_expected_time_to_start == 0 or res <= nb_available:
-                nb_available -= res
-                pjob_expected_time_to_start = -1
-                queue = queue[1:]
-            for (_, res, walltime, _, _) in queue:
-                if pjob_expected_time_to_start == -1 and res <= nb_available:
-                    nb_available -= res
-                elif res <= nb_available and walltime <= pjob_expected_time_to_start:
-                    nb_available -= res
+        reservation_size, sim_time, platform = 0, obs['time'], obs['platform']
+        nb_available = self._get_available_resources(obs)
 
-        reservation_size = 0
-        for i, node in enumerate(obs['platform']):
+        for i, node in enumerate(platform):
             if nb_available >= len(node):
                 if all(r == 0 for r in node):
-                    self.nodes_state[i] += obs['time'] - self.current_time
+                    self.nodes_state[i] += sim_time - self.current_time
                 else:
-                    self.nodes_state[i] = 0
+                    self.nodes_state[i] = -1
                 if self.nodes_state[i] >= self.timeout or all(r == 2 or r == 3 for r in node):
                     reservation_size += 1
                     nb_available -= len(node)
 
-        self.current_time = obs['time']
+        self.current_time = sim_time
         return reservation_size
 
     def play(self, env, render=False, verbose=False):
+        self.nodes_state = np.zeros(shape=self.nodes_state.shape)
         self.current_time = 0
         obs, done, score = env.reset(), False, 0
         while not done:
@@ -133,7 +63,8 @@ class TimeoutAgent(Agent):
                 env.render()
             obs, reward, done, info = env.step(self.act(obs))
             score += reward
-        results = pd.read_csv(os.path.join(GridEnv.OUTPUT, '_schedule.csv')).to_dict('records')[0]
+        results = pd.read_csv(os.path.join(
+            GridEnv.OUTPUT, '_schedule.csv')).to_dict('records')[0]
         results['score'] = score
         if verbose:
             m = " - ".join("[{}: {}]".format(k, v) for k, v in results.items())
--- src/timeout.py
+++ src/timeout.py
@@ -9,16 +9,14 @@ from gym import spaces
 
 from gridgym.envs.grid_env import GridEnv
 from gridgym.envs.simulator.utils.graphics import plot_simulation_graphics
-from src.agents.timeout import TimeoutAgent, TimeoutWithOracleAgent
+from src.agents.timeout import TimeoutAgent
 
 
 def run(args):
     env = gym.make(args.env_id)
-    nb_resources = env.observation_space.spaces['platform'].shape[0] * \
-        env.observation_space.spaces['platform'].shape[1]
+    nb_resources = env.observation_space.spaces['agenda'].shape[0]
     nb_nodes = env.observation_space.spaces['platform'].shape[0]
-    #agent = TimeoutWithOracleAgent(nb_resources, nb_nodes, t_turnoff=3, t_turnon=1, min_idle_time=60*5)
-    agent = TimeoutAgent(args.timeout, nb_resources, nb_nodes)
+    agent = TimeoutAgent(args.timeout, nb_resources, nb_nodes, args.queue_sz)
     results = agent.play(env, verbose=args.verbose, render=args.render)
 
     if args.plot_results:
@@ -29,10 +27,12 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--env_id", default="OffReservation-v0", type=str)
     parser.add_argument("--log_dir", default="../weights/", type=str)
-    parser.add_argument("--timeout", default=1, type=int)
     parser.add_argument("--plot_results", default=False, action="store_true")
     parser.add_argument("--verbose", default=True, action="store_true")
     parser.add_argument("--render", default=False, action="store_true")
+    # Agent specific args
+    parser.add_argument("--timeout", default=1, type=int)
+    parser.add_argument("--queue_sz", default=10, type=int)
     return parser.parse_args()
 
 
